b0980cfe25b2d02ed207dca5e87dbfee33c66176
ganglike248 <rudfkr248@gmail.com>
2026-08-10 15:58:05 +0900

    day1: 챌린지1

---
+++ src/components/practice/basic/SampleTwo.vue
@@ -0,0 +1,12 @@
+<script setup>
+// import { ref } from 'vue'
+const welcomeMessage = 'Welcome to Skala-Vue'
+</script>
+
+<template>
+  <div class="practice-section">
+    <h1>{{ welcomeMessage }}</h1>
+    <p>{{ welcomeMessage.toUpperCase() }}</p>
+    <p>{{ 'Random number: ' + Math.ceil(Math.random() * 100) }}</p>
+  </div>
+</template>
